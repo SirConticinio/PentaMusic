19059cbc6b7bb3af8421ee3c121ed091cbbf97db
alvarosg3 <alvarosang2003@gmail.com>
2023-09-26 10:32:50 +0200

    arreglos sql

--- src/main/python/pentamusic/basedatos/sql.py
+++ src/main/python/pentamusic/basedatos/sql.py
@@ -48,7 +48,7 @@ class SQL:
                                                 FOREIGN KEY (user) REFERENCES usuarios(user),
                                                 FOREIGN KEY (partitura) REFERENCES partitura(id)
                             )""")
-            self.c.execute("""CREATE TRIGGER IF NOT EXISTS insert_user_partitura
+            self.c.execute("""CREATE TRIGGER IF NOT EXISTS insert_partitura
                                 AFTER INSERT ON partitura
                                 FOR EACH ROW
                                 BEGIN
